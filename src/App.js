2d58eaaf6550be45e5743de9ce11f6e900335cc3
Tran Dinh Bao <trandinhbao321@gmail.com>
2023-03-20 15:58:34 +0700

    update final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import Head from "./components/Header/head";
-import Sidebar from "./components/sidebar/sidebar";
 
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Page from "./components/page/page";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Sidebar />} />
-				<Route path="/b" element={<Head />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/b" element={<Head />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
